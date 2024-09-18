71266cb115dc2ffce76ee101dbe94efa2e84456a
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-09-18 14:26:49 -0300

    feat(header): desserts text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
-  return (
-    <Flex
-      direction={{ base: "column", md: "row" }}
-      p={4}
-      justify="space-between"
-    >
-      <Box flex="3">
-        <Heading mb={6} fontSize={{ base: "2xl", md: "3xl" }}>
-          Desserts
-        </Heading>
-        <Text>Lista de produtos</Text>
-      </Box>
-    </Flex>
-  );
+  return <ProductList />;
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,1 +1,13 @@
-// Lista de produtos
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function ProductList() {
+  return (
+    <>
+      <Box flex="3">
+        <Heading mb={6} fontSize={{ base: "2xl", md: "3xl" }}>
+          Desserts
+        </Heading>
+      </Box>
+    </>
+  );
+}
