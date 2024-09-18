43fded292cc33f2a3334eba00fb04406a599ffab
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-09-17 22:09:04 -0300

    fix(folders): organize files and folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ProductList from "@/components/ProductList";
 import { Box, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box>
       <Text>Start project!</Text>
+      <ProductList />
     </Box>
   );
 }
---
+++ src/components/Cart.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Cart() {
+  return (
+    <Box>
+      <Text>Cart</Text>
+    </Box>
+  );
+}
