833f7da28c4f1dce7d17ed03eb1f23d98eb5b810
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-09-19 15:40:32 -0300

    feat(cart): init cart development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import Cart from "@/components/Cart";
 import ProductList from "@/components/ProductList";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Home() {
-  return <ProductList />;
+  return (
+    <Flex
+      direction={{ base: "column", md: "row" }}
+      p={4}
+      justify="space-between"
+    >
+      <Box flex="3">
+        <ProductList />
+      </Box>
+      <Box flex="1" ml={{ base: 0, md: 8 }} mt={{ base: 8, md: 0 }}>
+        <Cart />
+      </Box>
+    </Flex>
+  );
 }
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,13 +1,57 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex, IconButton } from "@chakra-ui/react";
+import { useCart } from "@/hooks/useCart";
+import Image from "next/image";
 
-export default function Cart() {
+const Cart = () => {
+  const { cartItems, removeFromCart } = useCart();
+
   return (
-    <>
-      <Box>
-        <Heading size="lg" mb={4}>
-          Your Cart quantidade de itens
-        </Heading>
-      </Box>
-    </>
+    <Box p={4} bg="gray.50" borderRadius="lg">
+      <Heading size="lg" mb={4} color="#c83b0e">
+        Your Cart ({cartItems.length})
+      </Heading>
+      {cartItems.length === 0 ? (
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          mb={4}
+        >
+          <Image
+            src="/assets/images/illustration-empty-cart.svg"
+            alt="Your cart"
+            width={150}
+            height={150}
+          />
+          <Text mt={4}>Your added items will appear here</Text>
+        </Flex>
+      ) : (
+        cartItems.map((item, index) => (
+          <Flex key={index} justify="space-between" mb={2}>
+            <Text>{item.name}</Text>
+            <Flex align="center">
+              <Text mr={4}>${item.price.toFixed(2)}</Text>
+              <IconButton
+                aria-label="Remove item"
+                isRound
+                border="2px solid"
+                icon={
+                  <Image
+                    src="/assets/images/icon-remove-item.svg"
+                    alt="Remove item"
+                    width={10}
+                    height={10}
+                  />
+                }
+                size="sm"
+                onClick={() => removeFromCart(item.name)}
+              />
+            </Flex>
+          </Flex>
+        ))
+      )}
+    </Box>
   );
-}
+};
+
+export default Cart;
