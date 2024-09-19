dbef3111043dac53de6d0f2d4b10808188674608
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-09-18 22:13:52 -0300

    feat(cart): init cart development

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,1 +1,13 @@
-// Cart
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function Cart() {
+  return (
+    <>
+      <Box>
+        <Heading size="lg" mb={4}>
+          Your Cart quantidade de itens
+        </Heading>
+      </Box>
+    </>
+  );
+}
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,15 +1,21 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import ProductCart from "./ProductCart";
+import Cart from "./Cart";
 
 export default function ProductList() {
   return (
-    <>
+    <Flex
+      direction={{ base: "column", md: "row" }}
+      p={4}
+      justify="space-between"
+    >
       <Box flex="3">
         <Heading mb={6} fontSize={{ base: "2xl", md: "3xl" }}>
           Desserts
         </Heading>
         <ProductCart />
+        <Cart />
       </Box>
-    </>
+    </Flex>
   );
 }
